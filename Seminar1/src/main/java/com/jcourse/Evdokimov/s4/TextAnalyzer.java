3f38b85e8d4e980072cae3466dc9d9877c1f0a59
Yarvis54 <gfdtk66@yandex.ru>
2017-12-23 11:55:37 +0700

    4 лаб

--- Seminar1/src/main/java/com/jcourse/Evdokimov/s4/TextAnalyzer.java
+++ Seminar1/src/main/java/com/jcourse/Evdokimov/s4/TextAnalyzer.java
@@ -8,13 +8,12 @@ import java.util.Map;
 
 public class TextAnalyzer {
     public static void main(String[] args) throws IOException {
-        //InputStreamReader readStream;
         Map<String,Integer> wordsMap = new HashMap<>();
-        int charArray[] = new int[1024];
-        int currentPos = 0;
+        double totalWord = 0;
 
         if(args.length!=2) {
             System.out.println("Expect two args");
+            return;
         }
         try(Reader readStream = new InputStreamReader(new BufferedInputStream(new FileInputStream(args[0])),"UTF-8")) {
            // readStream = new InputStreamReader(new BufferedInputStream(new FileInputStream("fefrw.csv")),"UTF-8");
@@ -26,20 +25,35 @@ public class TextAnalyzer {
                 else
                     if(tempString.length()!=0)
                     {
+                        totalWord++;
                         String toVerify = new String(tempString.toString());
+                        Integer tempCount = wordsMap.get(toVerify);
+                        if(tempCount == null)
+                            wordsMap.put(toVerify,1);
+                        else
+                            wordsMap.put(toVerify,wordsMap.get(toVerify)+1);
+                        /*
                         if(wordsMap.containsKey(toVerify))
                         {
                             wordsMap.put(toVerify,wordsMap.get(toVerify)+1);
                         }else{
                             wordsMap.put(toVerify,1);
-                        }
+                        }*/
                         tempString.setLength(0);
                     }
             }
+            if(tempString.length()!=0)
+            {
+                String toVerify = new String(tempString.toString());
+                if(wordsMap.containsKey(toVerify))
+                {
+                    wordsMap.put(toVerify,wordsMap.get(toVerify)+1);
+                }else{
+                    wordsMap.put(toVerify,1);
+                }
+                tempString.setLength(0);
+            }
         }
-
-        //дописать ранее чтение строк (с учетом последнего слова в конце файла)
-        //сделать сначала чтение количества слов и если нул, то реагируем на отсутствие слова
 
         List<Map.Entry<String,Integer>> allWords = new ArrayList<>(wordsMap.entrySet());
         allWords.sort((a,b) ->{
@@ -51,8 +65,13 @@ public class TextAnalyzer {
             }
             return rs;
         });
-        for(Map.Entry<String,Integer> allWord : allWords){
-            System.out.printf("%s\t%s%n",allWord.getKey(),allWord.getValue());  //%n - перевод строки в зависимости от платформы
+
+        try(Writer writeStream = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream(args[1])), "UTF-8")){
+            for(Map.Entry<String,Integer> allWord : allWords){
+
+                writeStream.write(String.format("%s,%d,%f%%%n",allWord.getKey(),allWord.getValue(),(double)allWord.getValue()/totalWord*100.00));
+                //System.out.printf("%s\t%s%n",allWord.getKey(),allWord.getValue());  //%n - перевод строки в зависимости от платформы
+            }
         }
     }
 }
@@ -62,3 +81,34 @@ public class TextAnalyzer {
 
 //WireShark - сниффер для отладки 5 лабы (сервера)
 //POSTMAN
+
+/*работа через поток
+
+try(BufferedReader reader = new BufferedReader(new InputStreamReader(
+    new BufferedInputStream(new FileInputStream(args[0]))))){
+        int[] count = {0};
+        reader
+            .lines()            //поток строк
+            .map(line -> line.split("[^\\pL\\pN]")
+            .flatMap(Arrays::stream)
+            .filter(s -> !s.isEmpty())
+            .map(word -> new SimpleEntry<>(word, 1))
+            .peek(a->count[0]++)
+            .collect(toMap(a->a, a->1,(a,b)->a+b))
+                        //.collect(toMap(SimpleEntry::getKey, pair -> pair.getValue(), (a, b) -> a+b))        //прекращение потока
+            .entrySet()                         //создание нового потока из мапы (collect)
+            .stream()   //создание нового потока из пар
+            .sorted((a, b) -> {
+                int rs = b.getValue().compareTo(a.getValue());
+                if (rs == 0) {
+                    rs = a.getKey().compareTo(b.getKey());
+                    }
+                return rs;
+                })
+            .forEach(word,count) -> System.out.println(word+": "count));
+
+
+
+
+    }
+ */
